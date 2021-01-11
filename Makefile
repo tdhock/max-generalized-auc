763c17757fca1682d2cd86932e9509136c8a382b
Toby Dylan Hocking <tdhock5@gmail.com>
2021-01-11 15:50:52 -0700

    change color

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+slides.pdf: figure-compare-hinge-loss.png
+	pdflatex slides
+figure-compare-hinge-loss.png: figure-compare-hinge-loss.R figure-compare-hinge-loss-data.csv
+	R --vanilla < $<
+figure-compare-hinge-loss-data.csv: figure-compare-hinge-loss-data.R
+	R --vanilla < $<
 figure-neuroblastomaProcessed-combinations-interactive/index.html: figure-neuroblastomaProcessed-combinations-interactive.R neuroblastomaProcessed.combinations.rds
 	R --vanilla < $<
 figure-neuroblastomaProcessed-combinations.png: figure-neuroblastomaProcessed-combinations.R neuroblastomaProcessed.combinations.rds
--- figure-compare-hinge-loss.R
+++ figure-compare-hinge-loss.R
@@ -1,50 +1,7 @@
 library(data.table)
 library(ggplot2)
 
-pred.vec <- seq(-3, 3, by=0.25)
-grid.dt <- data.table(expand.grid(
-  pos=pred.vec,
-  neg=pred.vec))
-positive.part <- function(x)ifelse(0<x, x, 0)
-err.dt <- rbind(
-  data.table(
-    min.log.lambda=c(-Inf, 0),
-    max.log.lambda=c(0, Inf),
-    fn=c(1, 0), possible.fn=1,
-    fp=c(0, 0), possible.fp=0,
-    label=1, obs="pos"),
-  data.table(
-    min.log.lambda=c(-Inf, 0),
-    max.log.lambda=c(0, Inf),
-    fn=c(0, 0), possible.fn=0,
-    fp=c(0, 1), possible.fp=1,
-    label=-1, obs="neg"))
-err.dt[, labels := 1]
-err.dt[, errors := fp+fn]
-lab.dt <- unique(err.dt[, .(label, obs)])
-loss.wide.list <- list()
-for(pred.i in 1:nrow(grid.dt)){
-  pred.wide <- grid.dt[pred.i]
-  pred.tall <- melt(
-    pred.wide,
-    measure.vars=lab.dt$obs,
-    value.name="pred.log.lambda",
-    variable.name="obs"
-  )[lab.dt, on="obs"]
-  pred.tall[, hinge.loss := positive.part(1-label*pred.log.lambda)]
-  pred.tall[, pred.label := ifelse(0<pred.log.lambda, 1, -1)]
-  pred.tall[, `01.loss` := ifelse(pred.label == label, 0, 1)]
-  roc.list <- penaltyLearning::ROChange(
-    err.dt, pred.tall,
-    problem.vars="obs")
-  loss.wide.list[[pred.i]] <- data.table(
-    pred.i, pred.wide,
-    aum=roc.list$aum,
-    auc=roc.list$auc,
-    `01.loss`=sum(pred.tall[["01.loss"]]),
-    hinge.loss=sum(pred.tall$hinge.loss))
-}
-loss.wide <- do.call(rbind, loss.wide.list)
+loss.wide <- data.table::fread("figure-compare-hinge-loss-data.csv")
 
 loss.wide[, neg.auc := -auc]
 loss.tall <- melt(
@@ -78,7 +35,7 @@ gg <- ggplot()+
   scale_fill_gradient(
     "Relative\nvalues",
     low="white",
-    high="red")+
+    high="violet")+
   coord_equal()+
   xlab("Real-valued prediction for positive label")+
   ylab("Real-valued prediction\nfor negative label")
