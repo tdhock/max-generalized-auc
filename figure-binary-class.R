0abe3053a0967daec9fbe5b2237fa600f036a31f
Toby Dylan Hocking <tdhock5@gmail.com>
2021-01-14 05:59:32 -0700

    binary class curves

--- figure-binary-class.R
+++ figure-binary-class.R
@@ -1,4 +1,5 @@
-source("packages.R")
+library(data.table)
+library(ggplot2)
 
 d <- function(min.log.lambda, fp, fn){
   data.table(min.log.lambda, fp, fn)
@@ -15,51 +16,36 @@ profile <- function(..., possible.fp, possible.fn, errors, labels){
     fp, fn, errors, possible.fp, possible.fn, labels)]
 }
 profile.list <- list(
-  less=profile(
+  negative=profile(
+    d(-Inf, 0, 0),
+    d(0, 1, 0)),
+  positive=profile(
     d(-Inf, 0, 1),
-    d(0, 1, 0)))
-pred.dt <- data.table(problem=1, pred.log.lambda=0)
-roc.dt.list <- list()
-auc.dt.list <- list()
-for(profile.i in seq_along(profile.list)){
-  p <- data.table(profile.list[[profile.i]], problem=1)
-  roc.list <- penaltyLearning::ROChange(p, pred.dt, problem.vars="problem")
-  model <- names(profile.list)[[profile.i]]
-  roc.dt.list[[profile.i]] <- data.table(model, roc.list$roc)
-  auc.dt.list[[profile.i]] <- with(roc.list, data.table(model, auc, aum))
-}
-roc.dt <- do.call(rbind, roc.dt.list)
-auc.dt <- do.call(rbind, auc.dt.list)
-fp.fn.dt <- data.table::melt(roc.dt, measure.vars=c("fp", "fn", "min.fp.fn"))
+    d(0, 0, 0)))
+profile.wide <- data.table(
+  label=names(profile.list)
+)[, profile.list[[label]], by=label]
 err.sizes <- c(
   fp=3,
-  fn=2,
-  min.fp.fn=1)
+  fn=2)
 err.colors <- c(
   fp="red",
-  fn="deepskyblue",
-  min.fp.fn="black")
+  fn="deepskyblue")
+profile.tall <- data.table::melt(
+  profile.wide,
+  measure=c("fp", "fn"))
 ggplot()+
-  facet_grid(model ~ ., labeller=label_both)+
+  facet_grid(label ~ ., labeller=label_both)+
   theme_bw()+
   theme(panel.spacing=grid::unit(0, "lines"))+
-  geom_rect(aes(
-    xmin=min.thresh, xmax=max.thresh,
-    ymin=0, ymax=value),
-    color="grey",
-    fill="grey",
-    data=fp.fn.dt[variable=="min.fp.fn"])+
   geom_segment(aes(
-    min.thresh, value,
+    min.log.lambda, value,
     color=variable, size=variable,
-    xend=max.thresh, yend=value),
-    data=fp.fn.dt)+
+    xend=max.log.lambda, yend=value),
+    data=profile.tall)+
   scale_color_manual(values=err.colors)+
   scale_size_manual(values=err.sizes)+
-  scale_x_continuous("prediction threshold")
+  scale_x_continuous("Predicted value f(x)")
 
-ggplot()+
-  geom_path(aes(
-    FPR, TPR, color=model, size=model, group=model),
-    data=roc.dt)
+
 
