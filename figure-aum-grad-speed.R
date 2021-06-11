9220f79fc2e5849a5522aa746f58f2a0c5bece7e
Toby Dylan Hocking <tdhock5@gmail.com>
2021-06-11 15:37:40 -0400

    random fig

--- figure-aum-grad-speed.R
+++ figure-aum-grad-speed.R
@@ -6,6 +6,27 @@ timing.stats <- timing.dt[, .(
   min=min(seconds),
   times=.N
 ), by=.(N, pred.type, algorithm)]
+some.stats <- timing.stats[pred.type=="pred.rnorm"]
+some.stats[, Algorithm := gsub("[.]", "\n", algorithm)]
+gg <- ggplot()+
+  geom_ribbon(aes(
+    N, ymin=min, ymax=max, fill=Algorithm),
+    alpha=0.5,
+    data=some.stats)+
+  geom_line(aes(
+    N, median, color=Algorithm),
+    data=some.stats)+
+  scale_x_log10(
+    "Number of predicted values",
+    limits=c(10, 12000),
+    breaks=c(10, 100, 1000, timing.stats[, max(N)]))+
+  scale_y_log10(paste0("Computation time in seconds,
+median line, min/max band
+over ",timing.stats[1, times], " timings"))
+dl <- directlabels::direct.label(gg, "right.polygons")
+png("figure-aum-grad-speed-random.png", width=4, height=3, res=200, units="in")
+print(dl)
+dev.off()
 gg <- ggplot()+
   geom_ribbon(aes(
     N, ymin=min, ymax=max, fill=algorithm),
