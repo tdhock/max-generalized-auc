b78524ca3db483250bba8873b03bb719a1559d48
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2022-02-03 13:10:34 -0700

    first sonar fig

--- figure-sonar-comparisons-data.R
+++ figure-sonar-comparisons-data.R
@@ -1,3 +1,4 @@
+library(data.table)
 data(package="mlbench")
 data(Sonar, package="mlbench")
 data(DNA, package="mlbench")
@@ -8,70 +9,6 @@ data.list <- list(
   DNA=list(
     input.mat=ifelse(as.matrix(DNA[,1:180])==0, 0, 1),
     output.vec=ifelse(DNA$Class=="n", -1, 1)))
-set.seed(1)
-library(data.table)
-PairsDT <- function(output.vec){
-  is.positive <- output.vec == 1
-  data.table(expand.grid(
-    positive=which(is.positive),
-    negative=which(!is.positive)))
-}
-equal.class.weights <- function(output.vec){
-  otab <- table(output.vec)
-  as.numeric(1/otab[paste(output.vec)])
-}
-Logistic <- function(pred.vec, output.vec, obs.weights){
-  list(
-    gradient=-obs.weights*output.vec/(1+exp(output.vec*pred.vec)),
-    loss=sum(obs.weights*log(1+exp(-output.vec*pred.vec))))
-}
-AUM <- function(pred.vec, diff.dt){
-  L <- aum::aum(diff.dt, pred.vec)
-  d <- L$derivative_mat
-  non.diff <- abs(d[,1] - d[,2]) > 1e-6
-  if(any(non.diff)){
-    cat(sprintf("%d non-diff points\n", sum(non.diff)))
-    print(d[non.diff, ])
-  }
-  with(L, list(gradient=rowMeans(derivative_mat), loss=aum))
-}
-loss.list <- list(
-  logistic=function(pred.vec, output.vec, ...){
-    Logistic(pred.vec, output.vec, 1/length(pred.vec))
-  },
-  logistic.weighted=
-    function(pred.vec, output.vec,
-             obs.weights=subtrain.obs.weights, ...){
-      Logistic(pred.vec, output.vec, obs.weights)
-    },
-  aum.count=function(pred.vec, diff.count.dt, ...){
-    AUM(pred.vec, diff.count.dt)
-  },
-  aum.rate=function(pred.vec, diff.rate.dt, ...){
-    AUM(pred.vec, diff.rate.dt)
-  },
-  squared.hinge.all.pairs=function(pred.vec, pairs.dt, margin=1, ...){
-    pairs.dt[, diff := pred.vec[positive]-pred.vec[negative]-margin]
-    pairs.dt[, diff.clipped := ifelse(diff<0, diff, 0)]
-    pairs.tall <- data.table::melt(
-      pairs.dt,
-      measure.vars=c("positive", "negative"),
-      value.name="pred.i",
-      variable.name="label")
-    ## d/dx (x - y - m)^2 = x - y - m
-    ## d/dy (x - y - m)^2 = -(x - y - m)
-    pairs.tall[, grad.sign := ifelse(label=="positive", 1, -1)]
-    N.pairs <- nrow(pairs.dt)
-    grad.dt <- pairs.tall[, .(
-      gradient=sum(grad.sign*diff.clipped)
-    ), keyby=pred.i]
-    list(
-      gradient=grad.dt$gradient/N.pairs,
-      loss=sum(pairs.dt$diff.clipped^2)/N.pairs)
-  }
-)
-
-out.loss.list <- list()
 data.name <- "Sonar"
 input.output.list <- data.list[[data.name]]
 input.mat <- input.output.list[["input.mat"]]
@@ -79,7 +16,69 @@ full.input.mat <- scale(input.mat)
 full.output.vec <- input.output.list[["output.vec"]]
 stopifnot(full.output.vec %in% c(-1, 1))
 unique.sets <- c("subtrain", "validation", "test")
-for(seed in 1:4){
+
+OneSeed <- function(seed){
+  PairsDT <- function(output.vec){
+    is.positive <- output.vec == 1
+    data.table(expand.grid(
+      positive=which(is.positive),
+      negative=which(!is.positive)))
+  }
+  equal.class.weights <- function(output.vec){
+    otab <- table(output.vec)
+    as.numeric(1/otab[paste(output.vec)])
+  }
+  Logistic <- function(pred.vec, output.vec, obs.weights){
+    list(
+      gradient=-obs.weights*output.vec/(1+exp(output.vec*pred.vec)),
+      loss=sum(obs.weights*log(1+exp(-output.vec*pred.vec))))
+  }
+  AUM <- function(pred.vec, diff.dt){
+    L <- aum::aum(diff.dt, pred.vec)
+    d <- L$derivative_mat
+    non.diff <- abs(d[,1] - d[,2]) > 1e-6
+    if(any(non.diff)){
+      cat(sprintf("%d non-diff points\n", sum(non.diff)))
+      print(d[non.diff, ])
+    }
+    with(L, list(gradient=rowMeans(derivative_mat), loss=aum))
+  }
+  loss.list <- list(
+    logistic=function(pred.vec, output.vec, ...){
+      Logistic(pred.vec, output.vec, 1/length(pred.vec))
+    },
+    logistic.weighted=
+      function(pred.vec, output.vec,
+               obs.weights=subtrain.obs.weights, ...){
+        Logistic(pred.vec, output.vec, obs.weights)
+      },
+    aum.count=function(pred.vec, diff.count.dt, ...){
+      AUM(pred.vec, diff.count.dt)
+    },
+    aum.rate=function(pred.vec, diff.rate.dt, ...){
+      AUM(pred.vec, diff.rate.dt)
+    },
+    squared.hinge.all.pairs=function(pred.vec, pairs.dt, margin=1, ...){
+      pairs.dt[, diff := pred.vec[positive]-pred.vec[negative]-margin]
+      pairs.dt[, diff.clipped := ifelse(diff<0, diff, 0)]
+      pairs.tall <- data.table::melt(
+        pairs.dt,
+        measure.vars=c("positive", "negative"),
+        value.name="pred.i",
+        variable.name="label")
+      ## d/dx (x - y - m)^2 = x - y - m
+      ## d/dy (x - y - m)^2 = -(x - y - m)
+      pairs.tall[, grad.sign := ifelse(label=="positive", 1, -1)]
+      N.pairs <- nrow(pairs.dt)
+      grad.dt <- pairs.tall[, .(
+        gradient=sum(grad.sign*diff.clipped)
+      ), keyby=pred.i]
+      list(
+        gradient=grad.dt$gradient/N.pairs,
+        loss=sum(pairs.dt$diff.clipped^2)/N.pairs)
+    }
+  )
+  seed.dt.list <- list()
   set.seed(seed)
   set.vec <- sample(rep(unique.sets, l=length(full.output.vec)))
   set.data.list <- list()
@@ -97,7 +96,7 @@ for(seed in 1:4){
   X.mat <- set.data.list$subtrain$input.mat
   for(loss.name in names(loss.list)){
     loss.grad.fun <- loss.list[[loss.name]]
-    for(step.size in c(0.01, 0.04, 0.1, 0.4, 1)){
+    for(step.size in 10^seq(-2,1,by=0.5)){
       cat(sprintf("seed=%d loss=%s step.size=%f\n", seed, loss.name, step.size))
       set.seed(1)
       weight.vec <- last.w <- rnorm(ncol(X.mat))
@@ -125,43 +124,50 @@ for(seed in 1:4){
         weight.vec <- loss.after.step[["new.weight"]]
         diff.w <- sum(abs(weight.vec-last.w))
         last.w <- weight.vec
-        set.loss.vec <- numeric()
-        for(set.name in names(set.data.list)){
-          set.data <- set.data.list[[set.name]]
-          set.loss <- loss.for.weight(weight.vec, set.data)
-          set.loss.vec[[set.name]] <- set.loss[["loss"]]
-          roc.df <- WeightedROC::WeightedROC(
-            set.loss[["pred"]],
-            set.data[["output.vec"]])
-          auc <- WeightedROC::WeightedAUC(roc.df)
-          out.dt <- data.table(
-            seed,
-            loss.name,
-            step.size,
-            iteration,
-            set.name,
-            auc,
-            loss.value=set.loss$loss)
-          for(aum.type in c("count", "rate")){
-            diff.name <- paste0("diff.", aum.type, ".dt")
-            aum.list <- aum::aum(set.data[[diff.name]], set.loss[["pred"]])
-            out.col <- paste0("aum.", aum.type)
-            out.dt[[out.col]] <- aum.list[["aum"]]
-          }
-          out.loss.list[[paste(
-            seed,
-            loss.name,
-            step.size,
-            iteration,
-            set.name
-          )]] <- out.dt
-          if(2000 < iteration || diff.w < 1e-6){
-            done <- TRUE
-          }
-        }#for(set.name
+        diverged <- !is.finite(diff.w)
+        if(!diverged){
+          for(set.name in names(set.data.list)){
+            set.data <- set.data.list[[set.name]]
+            set.loss <- loss.for.weight(weight.vec, set.data)
+            roc.df <- WeightedROC::WeightedROC(
+              set.loss[["pred"]],
+              set.data[["output.vec"]])
+            auc <- WeightedROC::WeightedAUC(roc.df)
+            out.dt <- data.table(
+              seed,
+              loss.name,
+              step.size,
+              iteration,
+              set.name,
+              auc,
+              loss.value=set.loss$loss)
+            for(aum.type in c("count", "rate")){
+              diff.name <- paste0("diff.", aum.type, ".dt")
+              aum.list <- aum::aum(set.data[[diff.name]], set.loss[["pred"]])
+              out.col <- paste0("aum.", aum.type)
+              out.dt[[out.col]] <- aum.list[["aum"]]
+            }
+            seed.dt.list[[paste(
+              seed,
+              loss.name,
+              step.size,
+              iteration,
+              set.name
+            )]] <- out.dt
+          }#for(set.name
+        }#if(!diverged
+        if(2000 < iteration || diverged || diff.w < 1e-6){
+          done <- TRUE
+        }
       }#while(!done
     }#for(step.size
   }#for(loss.name
-}#for(seed
+  do.call(rbind, seed.dt.list)
+}
+
+LAPPLY <- future.apply::future_lapply
+future::plan("multisession")
+out.loss.list <- LAPPLY(1:10, OneSeed)
 out.loss <- do.call(rbind, out.loss.list)
+
 data.table::fwrite(out.loss, "figure-sonar-comparisons-data.csv")
--- figure-sonar-comparisons.R
+++ figure-sonar-comparisons.R
@@ -1,15 +1,20 @@
 library(ggplot2)
 library(data.table)
-out.loss <- data.table::fread("figure-sonar-comparisons.csv")
+out.loss <- data.table::fread("figure-sonar-comparisons-data.csv")
 
 (max.valid.auc <- out.loss[
   set.name=="validation",
-  .SD[which.max(auc), .(iteration, auc, set.name)],
+  .SD[which.max(auc), .(iteration, auc, set.name, step.size)],
   by=.(seed, loss.name)])
+show.loss <- out.loss[set.name != "test"][
+  max.valid.auc[,.(seed,loss.name,step.size)],
+  on=.(seed, loss.name, step.size)
+]
+
 ggplot(,aes(
   iteration, auc, color=set.name))+
   geom_line(
-    data=out.loss)+
+    data=show.loss)+
   geom_point(
     shape=1,
     data=max.valid.auc)+
@@ -21,11 +26,31 @@ ggplot(,aes(
 
 test.loss <- out.loss[set.name=="test"]
 test.selected <- test.loss[
-  max.valid.auc[, .(seed, loss.name, iteration)],
-  on=.(seed, loss.name, iteration)]
+  max.valid.auc[, .(seed, loss.name, step.size, iteration)],
+  on=.(seed, loss.name, step.size, iteration)]
 ggplot()+
   geom_point(aes(
     auc, loss.name),
+    data=test.selected)
+
+test.selected.stats <- test.selected[, .(
+  median=median(auc),
+  q25=quantile(auc, 0.25),
+  q75=quantile(auc, 0.75)
+), by=.(loss.name)][order(median)]
+levs <- test.selected.stats$loss.name
+test.selected.stats[, Loss := factor(loss.name, levs)]
+test.selected[, Loss := factor(loss.name, levs)]
+ggplot()+
+  geom_segment(aes(
+    q25, Loss,
+    xend=q75, yend=loss.name),
+    data=test.selected.stats)+
+  geom_label(aes(
+    median, Loss, label="median"),
+    data=test.selected.stats)+
+  geom_point(aes(
+    auc, Loss),
     data=test.selected)
 
 test.wide <- dcast(
@@ -37,6 +62,13 @@ test.compare <- melt(
   id.vars=c("seed", "aum.count"),
   variable.name="other.loss.name",
   value.name="other.loss.auc")
+test.dt <- test.compare[, {
+  L <- t.test(
+    aum.count, other.loss.auc,
+    alternative="greater",
+    paired=TRUE)
+  L[c("statistic","p.value")]
+}, by=other.loss.name][order(p.value)]
 ggplot()+
   geom_abline(slope=1, intercept=0, color="grey")+
   theme_bw()+
@@ -46,3 +78,16 @@ ggplot()+
   coord_equal()+
   facet_grid(. ~ other.loss.name)
 
+gg <- ggplot()+
+  geom_segment(aes(
+    q25, Loss,
+    xend=q75, yend=loss.name),
+    data=test.selected.stats)+
+  geom_point(aes(
+    median, Loss),
+    data=test.selected.stats)+
+  scale_x_continuous(
+    "Test AUC")
+png("figure-sonar-comparisons.png", width=4, height=1.3, res=200, units="in")
+print(gg)
+dev.off()
