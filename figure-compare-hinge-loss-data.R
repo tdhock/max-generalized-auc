8a0fa0e668f4f63833f4a70c1469f5a4e501afef
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2022-07-19 11:40:34 -0700

    pairwise loss viz

--- figure-compare-hinge-loss-data.R
+++ figure-compare-hinge-loss-data.R
@@ -6,6 +6,12 @@ grid.dt <- data.table(expand.grid(
   pos=pred.vec,
   neg=pred.vec))
 positive.part <- function(x)ifelse(0<x, x, 0)
+sigmoid <- function(x)1/(1+exp(-x))
+hinge <- function(pred, label)positive.part(1-pred*label)
+hingeSet <- function(DT){
+  DT[, hinge.loss := hinge(pred.real, label)]
+  DT[, squared.hinge := hinge.loss^2 ]
+}
 err.dt <- rbind(
   data.table(
     min.log.lambda=c(-Inf, 0),
@@ -25,16 +31,28 @@ lab.dt <- unique(err.dt[, .(label, obs)])
 loss.wide.list <- list()
 for(pred.i in 1:nrow(grid.dt)){
   pred.wide <- grid.dt[pred.i]
+  pred.wide[, cat(sprintf(
+    "%4d / %4d %f %f\n",
+    pred.i, nrow(grid.dt),
+    pos, neg))]
   pred.tall <- melt(
     pred.wide,
     measure.vars=lab.dt$obs,
-    value.name="pred.log.lambda",
+    value.name="pred.real",
     variable.name="obs"
   )[lab.dt, on="obs"]
-  pred.tall[, hinge.loss := positive.part(1-label*pred.log.lambda)]
-  pred.tall[, logistic.loss := log(1+exp(-label*pred.log.lambda))]
-  pred.tall[, pred.label := ifelse(0<pred.log.lambda, 1, -1)]
+  hingeSet(pred.tall)
+  pred.tall[, logistic.loss := log(1+exp(-label*pred.real))]
+  pred.tall[, pred.label := ifelse(0<pred.real, 1, -1)]
   pred.tall[, `01.loss` := ifelse(pred.label == label, 0, 1)]
+  pred.tall[, pred.prob := sigmoid(pred.real)]
+  pairwise <- dcast(
+    melt(pred.tall, measure.vars=c("pred.prob","pred.real")),
+    variable ~ obs)
+  pairwise[, pred.real := pos-neg]
+  pairwise[, label := 1]
+  hingeSet(pairwise)
+  pred.tall[, pred.log.lambda := pred.real]
   roc.list <- penaltyLearning::ROChange(
     err.dt, pred.tall,
     problem.vars="obs")
@@ -46,14 +64,17 @@ for(pred.i in 1:nrow(grid.dt)){
   roc.marg <- penaltyLearning::ROChange(
     err.marg, pred.tall,
     problem.vars="obs")
-  loss.wide.list[[pred.i]] <- data.table(
+  out.row <- data.table(
     pred.i, pred.wide,
     AUM=roc.list$aum,
     AUC=roc.list$auc,
     AUM.margin=roc.marg$aum,
-    `01.loss`=sum(pred.tall[["01.loss"]]),
-    logistic.loss=sum(pred.tall$logistic.loss),
-    hinge.loss=sum(pred.tall$hinge.loss))
+    pairwise=dcast(
+      pairwise, NULL ~ variable, value.var=c("hinge.loss", "squared.hinge")))
+  for(out.col in c("01.loss","logistic.loss","hinge.loss","squared.hinge")){
+    out.row[[out.col]] <- sum(pred.tall[[out.col]])
+  }
+  loss.wide.list[[pred.i]] <- out.row
 }
 loss.wide <- do.call(rbind, loss.wide.list)
 data.table::fwrite(loss.wide, "figure-compare-hinge-loss-data.csv")
--- figure-compare-hinge-loss.R
+++ figure-compare-hinge-loss.R
@@ -4,7 +4,10 @@ library(ggplot2)
 loss.wide <- data.table::fread("figure-compare-hinge-loss-data.csv")
 loss.tall <- melt(
   loss.wide,
-  measure.vars = c("AUC", "AUM", "hinge.loss", "AUM.margin", "logistic.loss"),
+  measure.vars = c(
+    "AUC", "AUM", "AUM.margin", "logistic.loss",
+    grep("pairwise.*_", names(loss.wide), value=TRUE),
+    "squared.hinge", "hinge.loss"),
   variable.name="loss.name",
   value.name="loss.value")
 normalize <- function(x)(x-min(x))/(max(x)-min(x))
@@ -12,7 +15,9 @@ loss.tall[, loss.norm := normalize(loss.value), by=loss.name]
 rect.dt <- data.table(
   xmin=0, xmax=Inf,
   ymin=-Inf, ymax=0)
-loss.contour <- loss.tall[loss.name %in% c("AUM", "AUM.margin", "hinge.loss")]
+levs <- c("AUM", "AUM.margin", "hinge.loss")
+loss.contour <- loss.tall[loss.name %in% levs]
+loss.contour[, loss.fac := factor(loss.name, levs)]
 show.breaks <- seq(1, 7, by=1)
 gg <- ggplot()+
   geom_abline(
@@ -46,7 +51,7 @@ gg <- ggplot()+
     data=loss.contour)+
   theme_bw()+
   theme(panel.spacing=grid::unit(0, "lines"))+
-  facet_grid(. ~ loss.name)+
+  facet_grid(. ~ loss.fac)+
   scale_fill_gradient(
     "Loss\nvalues",
     low="white",
@@ -62,6 +67,29 @@ png(
   "figure-compare-hinge-loss-contours.png", 
   width=8, height=3, res=200, units="in")
 print(gg)
+dev.off()
+
+loss.line <- loss.tall[pos+neg==0 & grepl("pairwise",loss.name)]
+loss.line[, loss := sub("pred.prob","last.act=sigmoid",sub("pred.real","last.act=none",sub("pairwise[.]","",sub("_","\n",loss.name))))]
+gg <- ggplot()+
+  ggtitle("Comparing pairwise loss functions")+
+  geom_line(aes(
+    pos, loss.value, color=loss),
+    size=1,
+    data=loss.line)+
+  scale_x_continuous(
+    "Predicted score yhat for positive example",
+    breaks=seq(-3, 3, by=1))+
+  coord_cartesian(xlim=c(-5, 3),ylim=c(0,9))+
+  scale_y_continuous(
+    "Pairwise loss after last activation (a), Loss[a(yhat)-a(-yhat)]",
+    limits=c(0,10),
+    breaks=seq(0,8,by=2))
+dl <- directlabels::direct.label(gg, "left.polygons")
+png(
+  "figure-compare-hinge-loss-pairwise-line.png", 
+  width=5, height=5, res=200, units="in")
+print(dl)
 dev.off()
 
 log.aum <- loss.tall[loss.name %in% c("AUM","logistic.loss")]
@@ -111,6 +139,222 @@ gg <- ggplot()+
   ylab("Predicted score for negative label")
 png(
   "figure-compare-hinge-loss-contours-logistic.png", 
+  width=5.5, height=3, res=200, units="in")
+print(gg)
+dev.off()
+
+log.aum <- loss.tall[loss.name %in% c(
+  "pairwise.squared.hinge_pred.real",
+  "pairwise.squared.hinge_pred.prob")]
+log.aum[, last.layer.output := sub(".*[.]", "", loss.name)]
+show.breaks <- seq(0, 3.5, by=0.5)
+gg <- ggplot()+
+  ggtitle("Pairwise squared hinge loss functions")+
+  geom_abline(
+    slope=1, intercept=0, color="grey")+
+  geom_tile(aes(
+    pos, neg, fill=loss.norm),
+    data=log.aum)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=-Inf,y=-Inf,label="   correct rank"),
+    color="grey50",
+    vjust=1,
+    angle=45,
+    hjust=0)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=Inf,y=-Inf,label="correct \nlabel "),
+    hjust=1,
+    vjust=-0.2)+
+  geom_rect(aes(
+    xmin=xmin, xmax=xmax,
+    ymin=ymin, ymax=ymax),
+    fill=NA,
+    color="black",
+    data=rect.dt)+
+  metR::geom_contour2(aes(
+    pos, neg, z=loss.value, label=stat(level)),
+    breaks=show.breaks,
+    color="blue",
+    size=1,
+    data=log.aum)+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ last.layer.output, labeller=label_both)+
+  scale_fill_gradient(
+    "Relative\nloss\nvalues",
+    low="white",
+    high="red")+
+  coord_equal()+
+  geom_abline(aes(
+    intercept=intercept, slope=slope),
+    color="grey",
+    data=data.table(intercept=0, slope=1))+
+  xlab("Predicted score for positive label")+
+  ylab("Predicted score for negative label")
+png(
+  "figure-compare-hinge-loss-squared-pairwise-relative.png", 
+  width=5.5, height=3, res=200, units="in")
+print(gg)
+dev.off()
+
+show.breaks <- seq(0, 3.5, by=0.5)
+gg <- ggplot()+
+  ggtitle("Pairwise squared hinge loss functions")+
+  geom_abline(
+    slope=1, intercept=0, color="grey")+
+  geom_tile(aes(
+    pos, neg, fill=loss.value),
+    data=log.aum)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=-Inf,y=-Inf,label="   correct rank"),
+    color="grey50",
+    vjust=1,
+    angle=45,
+    hjust=0)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=Inf,y=-Inf,label="correct \nlabel "),
+    hjust=1,
+    vjust=-0.2)+
+  geom_rect(aes(
+    xmin=xmin, xmax=xmax,
+    ymin=ymin, ymax=ymax),
+    fill=NA,
+    color="black",
+    data=rect.dt)+
+  metR::geom_contour2(aes(
+    pos, neg, z=loss.value, label=stat(level)),
+    breaks=show.breaks,
+    color="blue",
+    size=1,
+    data=log.aum)+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ last.layer.output, labeller=label_both)+
+  scale_fill_gradient(
+    "Loss\nvalues",
+    low="white",
+    high="red")+
+  coord_equal()+
+  geom_abline(aes(
+    intercept=intercept, slope=slope),
+    color="grey",
+    data=data.table(intercept=0, slope=1))+
+  xlab("Predicted score for positive label")+
+  ylab("Predicted score for negative label")
+png(
+  "figure-compare-hinge-loss-squared-pairwise.png", 
+  width=5.5, height=3, res=200, units="in")
+print(gg)
+dev.off()
+
+log.aum <- loss.tall[loss.name %in% c(
+  "pairwise.hinge.loss_pred.real",
+  "pairwise.hinge.loss_pred.prob")]
+log.aum[, last.layer.output := sub(".*[.]", "", loss.name)]
+show.breaks <- seq(0, 1.75, by=0.25)
+gg <- ggplot()+
+  ggtitle("Pairwise linear hinge loss functions")+
+  geom_abline(
+    slope=1, intercept=0, color="grey")+
+  geom_tile(aes(
+    pos, neg, fill=loss.norm),
+    data=log.aum)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=-Inf,y=-Inf,label="   correct rank"),
+    color="grey50",
+    vjust=1,
+    angle=45,
+    hjust=0)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=Inf,y=-Inf,label="correct \nlabel "),
+    hjust=1,
+    vjust=-0.2)+
+  geom_rect(aes(
+    xmin=xmin, xmax=xmax,
+    ymin=ymin, ymax=ymax),
+    fill=NA,
+    color="black",
+    data=rect.dt)+
+  metR::geom_contour2(aes(
+    pos, neg, z=loss.value, label=stat(level)),
+    breaks=show.breaks,
+    color="blue",
+    size=1,
+    data=log.aum)+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ last.layer.output, labeller=label_both)+
+  scale_fill_gradient(
+    "Relative\nloss\nvalues",
+    low="white",
+    high="red")+
+  coord_equal()+
+  geom_abline(aes(
+    intercept=intercept, slope=slope),
+    color="grey",
+    data=data.table(intercept=0, slope=1))+
+  xlab("Predicted score for positive label")+
+  ylab("Predicted score for negative label")
+png(
+  "figure-compare-hinge-loss-hinge-pairwise-relative.png", 
+  width=5.5, height=3, res=200, units="in")
+print(gg)
+dev.off()
+
+show.breaks <- seq(0, 1.75, by=0.25)
+gg <- ggplot()+
+  ggtitle("Pairwise linear hinge loss functions")+
+  geom_abline(
+    slope=1, intercept=0, color="grey")+
+  geom_tile(aes(
+    pos, neg, fill=loss.value),
+    data=log.aum)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=-Inf,y=-Inf,label="   correct rank"),
+    color="grey50",
+    vjust=1,
+    angle=45,
+    hjust=0)+
+  geom_text(aes(
+    x,y,label=label),
+    data=data.table(x=Inf,y=-Inf,label="correct \nlabel "),
+    hjust=1,
+    vjust=-0.2)+
+  geom_rect(aes(
+    xmin=xmin, xmax=xmax,
+    ymin=ymin, ymax=ymax),
+    fill=NA,
+    color="black",
+    data=rect.dt)+
+  metR::geom_contour2(aes(
+    pos, neg, z=loss.value, label=stat(level)),
+    breaks=show.breaks,
+    color="blue",
+    size=1,
+    data=log.aum)+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ last.layer.output, labeller=label_both)+
+  scale_fill_gradient(
+    "Loss\nvalues",
+    low="white",
+    high="red")+
+  coord_equal()+
+  geom_abline(aes(
+    intercept=intercept, slope=slope),
+    color="grey",
+    data=data.table(intercept=0, slope=1))+
+  xlab("Predicted score for positive label")+
+  ylab("Predicted score for negative label")
+png(
+  "figure-compare-hinge-loss-hinge-pairwise.png", 
   width=5.5, height=3, res=200, units="in")
 print(gg)
 dev.off()
