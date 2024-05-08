87ef616af3cb86d3d57a32a7e4b2ddc5eea92177
Toby Dylan Hocking <toby.hocking@r-project.org>
2024-05-08 04:59:16 -0700

    use first class vs rest

--- data_Classif.R
+++ data_Classif.R
@@ -1,34 +1,40 @@
 library(ggplot2)
 library(data.table)
-csv.vec <- Sys.glob(file.path("data_Classif","*.csv"))
+(csv.vec <- Sys.glob(file.path("data_Classif","*.csv")))[2]
 result.list <- list()
 ref.list <- list()
 for(csv.i in seq_along(csv.vec)){
   classif.csv <- csv.vec[[csv.i]]
   data.name <- sub(".csv","",basename(classif.csv))
+  raw.dt <- fread(classif.csv)
   set.seed(1)
-  classif.dt <- fread(classif.csv)[sample(.N)]
+  classif.dt <- raw.dt[
+    sample(.N)
+  ][
+  , y := ifelse(y==y[1], 0, 1)
+  ][]
   group.name <- names(classif.dt)[1]
   feature.names <- setdiff(names(classif.dt),c(group.name,"y"))
   y.tab <- table(classif.dt[["y"]])
   max.N <- nrow(classif.dt)
   min.N <- 10*max.N/min(y.tab)
   N.vec <- as.integer(10^seq(log10(min.N),log10(max.N),by=0.25))
-  expr.list <- atime::atime_grid(
-    list(maxIt=c("min.aum","linear","quadratic")),
-    lm={
-      set.seed(1)
-      fit <- aum:::aum_linear_model(
-        feature.list, diff.list,
-        maxIterations=maxit.list[[maxIt]],
-        improvement.thresh=imp.thr)
-      data.table(
-        fit=list(fit),
-        sum.iterations=sum(fit$search$iterations),
-        steps=nrow(fit$search),
-        mean.it.per.step=mean(fit$search$iterations),
-        min.valid.AUM=fit$loss[set=="validation", min(aum)])
-    })
+  expr.list <- atime::atime_grid(list(
+    maxIt=c("min.aum","linear","quadratic"),
+    seed=1:4
+  ), lm={
+    set.seed(seed)
+    fit <- aum:::aum_linear_model(
+      feature.list, diff.list,
+      maxIterations=maxit.list[[maxIt]],
+      improvement.thresh=imp.thr)
+    data.table(
+      fit=list(fit),
+      sum.iterations=sum(fit$search$iterations),
+      steps=nrow(fit$search),
+      mean.it.per.step=mean(fit$search$iterations),
+      min.valid.AUM=fit$loss[set=="validation", min(aum)])
+  })
   result.list[[data.name]] <- atime::atime(
     N=N.vec,
     setup={
@@ -53,6 +59,7 @@ for(csv.i in seq_along(csv.vec)){
       imp.thr <- diff.list[[1]][, .(
         diff=c(fp_diff,fn_diff)
       )][diff!=0, min(abs(diff))/100]
+      print(imp.thr)
       N.subtrain <- nrow(feature.list$subtrain)
       maxit.list <- list(
         min.aum="min.aum",
